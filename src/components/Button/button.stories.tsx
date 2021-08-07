8ddae4b8f31d852c40d18d7bf1719e153b56cbc5
fujia <fujia.site@outlook.com>
2021-08-07 16:30:26 +0800

    prepare for publish&config project

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Button, { ButtonProps }  from './button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {}
+};
+
+// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Button</Button>
+export const Default: React.VFC<ButtonProps> = () => <Button onClick={action('clicked')}>Default Button</Button>;
+
+export const Primary: React.VFC<ButtonProps> = () => <Button btnType="primary" onClick={action('clicked')}>Primary Button</Button>;
+
+export const Size: React.VFC<ButtonProps> = () => (
+  <>
+    <Button size="large" onClick={action('clicked')}>Large Button</Button>
+    <Button size="middle" onClick={action('clicked')}>Middle Button</Button>
+    <Button size="small" onClick={action('clicked')}>Small Button</Button>
+  </>
+);
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+export { default as Button } from './components/Button'
