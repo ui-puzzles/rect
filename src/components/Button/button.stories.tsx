326d4ad4aa50dcc2b7e3fc662feb485b2bd6b6ff
fujia <fujia.site@outlook.com>
2021-09-16 10:12:33 +0800

    feat: remove futile code

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Meta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Button, { ButtonProps }  from './button';
@@ -7,9 +7,10 @@ export default {
   title: 'Button',
   component: Button,
   argTypes: {}
-};
+} as Meta;
 
 // const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Button</Button>
+
 export const Default: React.VFC<ButtonProps> = () => <Button onClick={action('clicked')}>Default Button</Button>;
 
 export const Primary: React.VFC<ButtonProps> = () => <Button btnType="primary" onClick={action('clicked')}>Primary Button</Button>;
