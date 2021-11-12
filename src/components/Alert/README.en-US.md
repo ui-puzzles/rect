2449aeb0fdef920503183acca3a9d12460cda8cd
fujia <fujia.site@outlook.com>
2021-11-12 13:10:55 +0800

    feat: finish Alert basic function

--- src/components/Alert/README.en-US.md
+++ src/components/Alert/README.en-US.md
@@ -2,16 +2,36 @@
 
 The table to display the props of your component
 
-|Property|Description|Type|DefaultValue|
+|Property|Description|Type|Default|Version|
 |---|---|---|---|
-|  |  |  |  |
+| title* | title of Alert | ReactNode | - | 0.1.4 |
+| content | content of Alert | ReactNode | - | 0.1.4 |
+| customIcon | custom icon | ReactNode | - | 0.1.4 |
+| iconSize | custom icon size | SizeProp('xs' \| 'lg' \| 'sm' \| '1x' ... '10x') | '1x' | 0.1.4 |
+| showIcon | whether to display icon | boolean | false | 0.1.4 |
+| closable | whether can hidden Alert by close action | boolean  | false  | 0.1.4 |
+| hidden | whether to display Alert | boolean | false | 0.1.4 |
+| bordered | set border style | boolean | false | 0.1.4 |
+| extra | custom content ro render instead of close icon | ReactNode | - | 0.1.4 |
+| type | set Alert theme quickly | 'success' \| 'info' \| 'warning' \| 'error' | 'info' | 0.1.4 |
+| className | additional css class | string | - | 0.1.4 |
+| style | additional style | CSSProperties | - | 0.1.4 |
+| onClose | Callback when Alert is closed | (e: MouseEvent) => void | - | 0.1.4 |
 
 ## Change Log
 
-### 0.1.0
+### 0.1.4
 
-2021-09-12
+2021-09-11
 
 #### init
 
 - init template.
+
+2021-09-11
+
+#### develop
+
+- finish basic function.
+- added storybook.
+
--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,18 +1,108 @@
-import React, { FC } from 'react';
+import React, { FC, isValidElement, MouseEvent, useState } from 'react';
+import classnames from 'classnames';
 
-import { TemplateProps } from './interface';
+import { AlertProps } from './interface';
+import Icon, { IconProp } from '../Icon';
+import Transition from '../Transition';
+import { isFunc } from '../../utils';
 
+const prefixCls = 'pr-alert';
 
-const Template: FC<TemplateProps> = (props) => {
+const Alert: FC<AlertProps> = (props) => {
   const {
-    someProps
+    customIcon,
+    iconSize = '1x',
+    showIcon,
+    title,
+    content,
+    hidden = false,
+    closable = false,
+    bordered = false,
+    extra,
+    type = 'info',
+    className,
+    style,
+    onClose,
   } = props;
+  const [isHidden, setHidden] = useState(hidden);
+
+  const classNames = classnames(prefixCls, {
+    [`${prefixCls}-${type}`]: type, // the default value is "info"
+    [`${prefixCls}-bordered`]: bordered, // the default value is "left"
+  }, className);
+
+  const handleClose = (e: MouseEvent) => {
+    setHidden(true);
+    if (onClose && isFunc(onClose)) {
+      onClose(e);
+    }
+  };
+
+  const renderIcon = () => {
+    if (isValidElement(customIcon)) return (
+      <div className={`${prefixCls}-icon`}>
+        {customIcon}
+      </div>
+    );
+
+    const typeMapIcon = {
+      info: 'info-circle',
+      success: 'check-circle',
+      warning: 'exclamation-circle',
+      danger: 'times-circle'
+    };
+
+    if (showIcon) {
+      return (
+        <div className={`${prefixCls}-icon`}>
+          <Icon icon={typeMapIcon[type] as IconProp} size={iconSize} theme={type} />
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  const renderSuffix = () => {
+    if (closable && !extra) {
+      return (
+        <div className={`${prefixCls}-suffix`}>
+          <Icon icon="times" onClick={handleClose} />
+        </div>
+      );
+      /**
+      * NOTE: if users past the props of "extra" and want to hidden
+      * the component. we think to do this in the "extra" by users is better practice
+      */
+    } else if (isValidElement(extra)) {
+      return (
+        <div className={`${prefixCls}-suffix`}>
+          {extra}
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
-    <div>
-      {someProps}
-    </div>
+    <Transition
+      in={!isHidden && !hidden}
+      timeout={300}
+      animation="zoom-in-top"
+    >
+      <div className={classNames} style={style}>
+        {renderIcon()}
+        <div className={`${prefixCls}-wrapper`}>
+          <div className={`${prefixCls}-title`}>{title}</div>
+          {
+            content && <div className={`${prefixCls}-content`}>{content}</div>
+          }
+        </div>
+        {renderSuffix()}
+      </div>
+    </Transition>
   );
 };
 
-export default Template;
+export default Alert;
