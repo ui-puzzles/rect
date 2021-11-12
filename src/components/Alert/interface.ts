ee2dd1edca36f7bc247a751342cd86f4dc9210ef
fujia <fujia.site@outlook.com>
2021-11-12 13:10:55 +0800

    feat: finish Alert basic function

--- src/components/Alert/interface.ts
+++ src/components/Alert/interface.ts
@@ -1,6 +1,19 @@
-/**
- * the types for your component
- */
-export interface TemplateProps {
-  someProps: 'propValue'
+import { ReactNode, MouseEvent, CSSProperties } from "react";
+import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core';
+
+export type AlertType = 'success' | 'info' | 'warning' | 'danger';
+export interface AlertProps {
+  title: ReactNode;
+  content?: ReactNode;
+  customIcon?: IconProp;
+  iconSize?: SizeProp;
+  showIcon?: boolean;
+  closable?: boolean;
+  hidden?: boolean;
+  bordered?: boolean;
+  extra?: ReactNode;
+  type?: AlertType;
+  className?: string;
+  style?: CSSProperties;
+  onClose?: (e: MouseEvent) => void;
 }
