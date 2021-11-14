1df31f3fda294e6ae0b0a0369f48f259737ca7af
fujia <fujia.site@outlook.com>
2021-11-14 22:18:45 +0800

    feat: develop Card component

--- src/components/Card/interface.ts
+++ src/components/Card/interface.ts
@@ -1,6 +1,33 @@
-/**
- * the types for your component
- */
-export interface TemplateProps {
-  someProps: 'propValue'
+import { ReactNode, MouseEvent, CSSProperties } from "react";
+
+export interface CardProps {
+  className?: string;
+  style?: CSSProperties;
+  title?: ReactNode;
+  extra?: ReactNode;
+  headerStyle?: CSSProperties;
+  width?: number;
+  minHeight?: number;
+  bodyStyle?: CSSProperties;
+  hoverEffect?: boolean;
+  bordered?: boolean;
+  loading?: boolean;
+  cover?: ReactNode;
+  coverMode?: 'normal' | 'fill';
+  isCoverBlur?: boolean;
+  footer?: ReactNode;
+}
+
+
+export interface CardGridProps {
+  className?: string;
+  style?: CSSProperties;
+}
+
+export interface CardMetaProps {
+  className?: string;
+  style?: CSSProperties;
+  avatar?: ReactNode;
+  title?: ReactNode
+  description?: ReactNode
 }
