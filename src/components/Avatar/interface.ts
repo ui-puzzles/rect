8e2c9f294d38e6b08d3dd5b0bf9bc203a6dcf5e9
fujia <fujia.site@outlook.com>
2021-11-14 09:45:05 +0800

    feat: complete basic functions&added docs

--- src/components/Avatar/interface.ts
+++ src/components/Avatar/interface.ts
@@ -1,6 +1,13 @@
-/**
- * the types for your component
- */
-export interface TemplateProps {
-  someProps: 'propValue'
+import { ReactNode, MouseEvent, CSSProperties } from "react";
+
+export interface AvatarProps {
+  className?: string;
+  style?: CSSProperties;
+  shape?: 'circle' | 'square';
+  size?: number;
+  autoFitFontSize?: boolean;
+  extra?: ReactNode;
+  extraStyle?: CSSProperties;
+  extraType?: 'widget' | 'mask';
+  onClick?: (e: MouseEvent) => void;
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -16,3 +16,27 @@ export const warning = (msg: string) => {
 };
 
 export const isDev = (() => process.env.NODE_ENV !== 'production')();
+
+export /**
+ * @description auto adjust text's font size dependent parent's element.
+ *
+ * @param {HTMLElement} textElem text element, NOTE: can't be an inline-element
+ * @param {HTMLElement} wrapper wrapper of text element
+ * @param {number} [wrapperSize] specify the wrapper size
+ * @param {number} [offset=8] specify gap of text
+ */
+const  autoFixFontSize = (
+  textElem: HTMLElement | null,
+  wrapper: HTMLElement | null,
+  wrapperSize?: number,
+  offset = 8) => {
+  if (textElem) {
+    const textWidth = textElem.clientWidth;
+    const size = wrapperSize || wrapper?.offsetWidth || 0;
+    const scale = size / (textWidth + offset);
+
+    if (size && scale < 1) {
+      textElem.style.transform = `scale(${scale}) translateX(-50%)`;
+    }
+  }
+};
