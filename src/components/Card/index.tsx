131e92394a8baadf684ba49b136c677cc77b89e3
fujia <fujia.site@outlook.com>
2021-11-15 20:25:23 +0800

    feat: complete basic functions

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,19 @@
 
+import { FC } from 'react';
+
 import Card from './card';
+import Meta from './meta';
+import Grid from './grid';
+import { CardProps, CardGridProps, CardMetaProps } from './interface';
 
-export type { CardProps, CardGridProps, CardMetaProps } from './interface';
+export type ICardComponent = FC<CardProps> & {
+  Meta: FC<CardMetaProps>;
+  Grid: FC<CardGridProps>;
+};
 
-export default Card;
+const ComponentCard = Card as ICardComponent;
+
+ComponentCard.Meta = Meta;
+ComponentCard.Grid = Grid;
+
+export default ComponentCard;
