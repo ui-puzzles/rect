cf25c2cc1c95bc92c9a90acc7d87bb64891bac7e
fujia <fujia.site@outlook.com>
2021-11-28 16:40:47 +0800

    feat: eject config&added examples folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,5 @@ module.exports = {
         "@typescript-eslint/no-explicit-any": 0,
         "react/prop-types": 0,
     },
-    "ignorePatterns": ["docs"]
+    "ignorePatterns": ["docs", "scripts", "config"]
 };
---
+++ examples/src/app.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/index';
+import '@/styles/index.scss';
+
+function App() {
+  return (
+    <section>
+      <h1>ui-puzzles/rect</h1>
+      <Button label="Submit" />
+    </section>
+  );
+}
+
+export default App;
