c7214789b46c076395b0dbec49b61c07f47161ca
fujia <fujia.site@outlook.com>
2022-02-09 18:25:25 +0800

    fix: remove tabs components

--- src/components/Tabs/tabs.test.tsx
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { render } from "@testing-library/react";
-
-describe("Tabs", () => {
-  it("render default correctly", () => {});
-});
