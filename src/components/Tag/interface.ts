2fbce36f2324a5cb58cc2d956b53e31ff4b23008
fujia <fujia.site@outlook.com>
2022-03-20 15:04:19 +0800

    feat: finished basic functionalities

--- src/components/Tag/interface.ts
+++ src/components/Tag/interface.ts
@@ -1,6 +1,19 @@
-import { CSSProperties } from "react";
+import { CSSProperties, ReactNode } from 'react';
+
+type TagSize = 'xs' | 'sm' | 'md' | 'lg';
 
 export interface TagProps {
   className?: string;
   style?: CSSProperties;
+  bordered?: boolean;
+  size?: TagSize;
+  closable?: boolean;
+  selectable?: boolean;
+  visible?: boolean;
+  selected?: boolean;
+  defaultSelected?: boolean;
+  icon?: ReactNode;
+  closeIcon?: ReactNode;
+  onClose?: () => Promise<any> | void;
+  onSelect?: (checked: boolean) => void;
 }
