83b72d9bfb2eefaa042d3b59fdaf0a08b406495d
fujia <fujia.site@outlook.com>
2022-03-28 20:31:33 +0800

    feat: finished basic functionality

--- src/components/Pagination/interface.ts
+++ src/components/Pagination/interface.ts
@@ -1,6 +1,20 @@
-import { CSSProperties } from "react";
+import { CSSProperties } from 'react';
 
+export type PaginationSize = 'small' | 'default' | 'large';
 export interface PaginationProps {
   className?: string;
   style?: CSSProperties;
+  defaultCurrent: number;
+  current?: number;
+  defaultPageSize: number;
+  pageSize?: number;
+  total: number;
+  hideOnSinglePage?: boolean;
+  size?: PaginationSize;
+  onChange?: (pageNo: number, pageSize: number) => void;
+}
+
+export enum JumpAction {
+  Forward = -2,
+  Back,
 }
--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,7 @@ export { default as Input } from './components/Input';
 export { default as Layout } from './components/Layout';
 export { default as Menu } from './components/Menu';
 export { default as PageHeader } from './components/PageHeader';
+export { default as Pagination } from './components/Pagination';
 export { default as Progress } from './components/Progress';
 export { default as Tabs } from './components/Tabs';
 export { default as Tag } from './components/Tag';
